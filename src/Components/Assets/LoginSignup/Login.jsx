c738792d0aad45c555973efbf703d5e8d496a981
TriStrac <borloloy777leigh@gmail.com>
2024-07-13 00:01:28 +0800

    added button feature

--- src/Components/Assets/LoginSignup/Login.jsx
+++ src/Components/Assets/LoginSignup/Login.jsx
@@ -1,22 +1,36 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './LoginSignUp.css'
 
 export const Login = () => {
+    const[email, setEmail] = useState('')
+    const[password,setPassword] = useState('')
+
+    function loginUser(){
+        alert(`${email}: ${password}`)
+    }
+
+    function getEmail(val){
+        setEmail(val.target.value)
+    }
+    function getPassword(val){
+        setPassword(val.target.value)
+    }
+    
   return (
     <div className="container">
         <div className="header">
             <div className="text">Login</div>
             <div className="underline"></div>
-    </div>
+        </div>
         <div className="inputs">
             <div className='input'>
-                <input type="email" placeholder='Email'/>
+                <input type="email" placeholder='Email' onChange={getEmail}/>
             </div>
             <div className='input'>
-                <input type="password" placeholder='Password'/>
+                <input type="password" placeholder='Password' onChange={getPassword}/>
             </div>
             
-            <div className='submit'>Login</div>
+            <div className='submit' onClick={loginUser}>Login</div>
         </div>
     </div>
     
--- src/Components/Assets/LoginSignup/SignUp.jsx
+++ src/Components/Assets/LoginSignup/SignUp.jsx
@@ -1,50 +1,80 @@
-import React from 'react'
-import './LoginSignUp.css'
+import React, { useState } from 'react';
+import './LoginSignUp.css';
 
 export const SignUp = () => {
-  return (
-    <div className="container">
-        <div className="header">
-            <div className="text">Sign Up</div>
-            <div className="underline"></div>
-    </div>
-        <div className="inputs">
-            <div className='input'>
-                <input type="email" placeholder='Email'/>
+    const [formData, setFormData] = useState({
+        email: '',
+        password: '',
+        firstName: '',
+        middleName: '',
+        lastName: '',
+        birthDate: '',
+        street: '',
+        barangay: '',
+        province: '',
+        city: '',
+        type: ''
+    });
+
+    const handleChange = (e) => {
+        const { name, value } = e.target;
+        setFormData({
+            ...formData,
+            [name]: value
+        });
+    };
+
+    const handleSubmit = () => {
+        // Add your form submission logic here
+        alert(JSON.stringify(formData, null, 2));
+    };
+
+    return (
+        <div className="container">
+            <div className="header">
+                <div className="text">Sign Up</div>
+                <div className="underline"></div>
             </div>
-            <div className='input'>
-                <input type="password" placeholder='Password'/>
+            <div className="inputs">
+                <div className='input'>
+                    <input type="email" name="email" placeholder='Email' onChange={handleChange} />
+                </div>
+                <div className='input'>
+                    <input type="password" name="password" placeholder='Password' onChange={handleChange} />
+                </div>
+                <div className='input'>
+                    <input type="text" name="firstName" placeholder='First Name' onChange={handleChange} />
+                </div>
+                <div className='input'>
+                    <input type="text" name="middleName" placeholder='Middle Name' onChange={handleChange} />
+                </div>
+                <div className='input'>
+                    <input type="text" name="lastName" placeholder='Last Name' onChange={handleChange} />
+                </div>
+                <div className='input'>
+                    <input type="date" name="birthDate" placeholder='Birth Date' onChange={handleChange} />
+                </div>
+                <div className='input'>
+                    <input type="text" name="street" placeholder='Street' onChange={handleChange} />
+                </div>
+                <div className='input'>
+                    <input type="text" name="barangay" placeholder='Barangay' onChange={handleChange} />
+                </div>
+                <div className='input'>
+                    <input type="text" name="province" placeholder='Province' onChange={handleChange} />
+                </div>
+                <div className='input'>
+                    <input type="text" name="city" placeholder='City' onChange={handleChange} />
+                </div>
+                <div className='input'>
+                    <select name="type" onChange={handleChange}>
+                        <option value="">Select Type</option>
+                        <option value="owner">Owner</option>
+                        <option value="customer">Customer</option>
+                    </select>
+                </div>
+                <div className='submit' onClick={handleSubmit}>Sign Up</div>
             </div>
-            <div className='input'>
-                <input type="text" placeholder='First Name'/>
-            </div>
-            <div className='input'>
-                <input type="text" placeholder='Middle Name'/>
-            </div>
-            <div className='input'>
-                <input type="text" placeholder='Last Name'/>
-            </div>
-            <div className='input'>
-                <input type="date" placeholder='Birth Date'/>
-            </div>
-            <div className='input'>
-                <input type="text" placeholder='Street'/>
-            </div>
-            <div className='input'>
-                <input type="text" placeholder='Barangay'/>
-            </div>
-            <div className='input'>
-                <input type="text" placeholder='Province'/>
-            </div>
-            <div className='input'>
-                <input type="text" placeholder='City'/>
-            </div>
-            <div className='input'>
-                <input type="text" placeholder='Type'/>
-            </div>
-            <div className='submit'>Sign Up</div>
         </div>
-    </div>
-    
-  )
-}
+    );
+};
